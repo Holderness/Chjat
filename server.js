bdd9b8b9de769ed244bdba71f19bb0a66d3acb21
Holderness <andersonholderness@gmail.com>
2015-04-15 21:58:41 -0400

    socket.io instated

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
-var express = require('express');
-var app = express();
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+// app.use(express.static(__dirname + '/public'));
+
+// app.set('view engine', 'html');
+
+// app.get('/', function(req, res){
+//   res.render('index');
+// });
 
 
 
 
-app.use(express.static(__dirname + '/public'));
 
-app.set('view engine', 'html');
-
-app.get('/test', function(req, res){
-  res.render('test');
+app.get('/', function(req, res){
+  res.sendfile('test.html');
 });
 
-app.listen(3000, function(){
+io.on('connection', function(socket){
+  console.log('mothafucka, you connected');
+});
+
+http.listen(3000, function(){
   console.log('listening on *:3000');
 });
