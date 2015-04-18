b848c1a91c062efae61fd35e4224e041a5b6e98c
Evan Turner <evturn@gmail.com>
2015-04-18 13:39:12 -0400

    index now rendering on get to /

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-// app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
-// app.set('view engine', 'html');
-
-// app.get('/', function(req, res){
-//   res.render('index');
-// });
-
+app.set('view engine', 'html');
 
 app.get('/', function(req, res){
-  res.sendfile('test.html');
+  res.render('index');
 });
 
 io.on('connection', function(socket){
