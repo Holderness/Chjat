983b675b5f3fd714ca374a53c30b50f1b081e5e1
Holderness <andersonholderness@gmail.com>
2015-10-02 12:18:18 -0400

    add testing mongodb connection

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,17 @@ var config = require('./config'),
     mongoose = require('mongoose');
 
 module.exports = function() {
-  var db = mongoose.connect(config.db);
+  var db = mongoose.connect(config.db, function(err, db) {
+           console.log("test");
+           if (!err) {
+                   console.log("test");
+           }
+           else {
+                   console.dir(err);
+           throw err;
+           }
+   //  db.close();
+   });
 
   require('../app/models/user');
   require('../app/models/chatroom');
