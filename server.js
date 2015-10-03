ec6b0c7ccaf826b6f9874682221a98bae05061a1
Holderness <andersonholderness@gmail.com>
2015-10-03 13:07:11 -0400

    add io.configure

--- server.js
+++ server.js
@@ -28,10 +28,11 @@ var db = mongoose(),
   io.use(sharedsession(app.sessionStore, {
     autoSave: true
   }));
-
-  io.set('transports', ['xhr-polling']);
-  io.set('polling duration', 10);
-
+  io.configure(function() {
+    io.set('transports', ['xhr-polling']);
+    io.set('polling duration', 10);
+    io.set("close timeout", 10);
+  });
   // io.use(function(req, res, next) {
   //   console.log('>>>>>>>>>>>>>>>>>>>>>REQ<<<<<<<<<<<<', req.handshake);
   //   console.log('>>>>>>>>>>>>>>>>>>>>>RES<<<<<<<<<<<<', res);
